2e37d92c0c4131fcf7146ea427581684bffda7ba
tobiasorth@gmx.net <tobiasorth@gmx.net@f647d1a1-7bdd-de13-0d76-d4b52cfee2a1>
2011-05-24 11:00:04 +0000

    first (uncomplete) version of user stack setup

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -8,6 +8,7 @@
 #include "threads/interrupt.h"
 #include "threads/intr-stubs.h"
 #include "threads/palloc.h"
+#include "threads/malloc.h"
 #include "threads/switch.h"
 #include "threads/synch.h"
 #include "threads/vaddr.h"
