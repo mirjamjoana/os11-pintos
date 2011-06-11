e95488d49b0c46244554489ccc9ba8e9a8c796c5
mirjam.neuweigand@gmail.com <mirjam.neuweigand@gmail.com@f647d1a1-7bdd-de13-0d76-d4b52cfee2a1>
2011-06-11 18:31:42 +0000

    is_legal_stack_growth implementiert

--- src/vm/page.c
+++ src/vm/page.c
@@ -2,6 +2,8 @@
 #include "vm/frame.h"
 #include "threads/palloc.h"
 #include <debug.h>
+#include "threads/vaddr.h"
+#include "threads/thread.h"
 
 /* supplemental page table */
 
@@ -23,4 +25,35 @@ void free_multiple_user_pages(void * pages, size_t page_cnt){
 
 void *create_lazy_user_page (struct file* file UNUSED, uint32_t offset UNUSED, uint32_t length UNUSED) {
 	return NULL;
+}
+
+static bool
+is_legal_stack_growth (void **esp)
+{	
+	bool success = false;
+	struct thread *current_thread = thread_current();
+	/* checks whether there is enough space left (less than 8MB occupied) */
+	if ((PHYS_BASE - *esp) > 800000) {
+		current_thread->exit_status = -1;
+		thread_exit();
+	}
+
+	/* check whether it is an illegal push operation (more than 32 bytes beyond (PHYS_BASE - 8MB)) */
+	if ( (*esp - 20) < (PHYS_BASE - 800000 )) {
+		current_thread->exit_status = -1;
+		thread_exit();
+	}
+
+	uint8_t *kpage;
+	kpage = get_user_page (PAL_ZERO);
+	if (kpage != NULL) 
+	  {
+	    success = install_page (((uint8_t *) *esp) - PGSIZE, kpage, true);
+	    if (success)
+		*esp = *esp + PGSIZE;
+	    else
+	    	free_user_page (kpage);
+	  }
+	return success;
+ 	
 }
