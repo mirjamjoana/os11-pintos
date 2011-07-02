750a2f6535eae4e72df1c13f95b7855bda5751d8
tobiasorth@gmx.net <tobiasorth@gmx.net@f647d1a1-7bdd-de13-0d76-d4b52cfee2a1>
2011-07-02 16:24:39 +0000

    first version of cache, read-ahead tbi

--- trunk/src/filesys/cache.h
+++ trunk/src/filesys/cache.h
@@ -6,29 +6,30 @@
 #include <stdlib.h>
 #include "devices/block.h"
 #include "threads/synch.h"
-
-#define debug 0
 #include <stdio.h>
 
-void cache_read (block_sector_t bid, void * buffer, int offset, int readsize);
+#define CACHE_READ 0
+#define CACHE_WRITE 1
+#define CACHE_SIZE 64
+
 void cache_init (void);
+void cache_flush (void);
 
-void cache_write (block_sector_t bid, void * buffer, int offset, int writesize);
-void cache_save_cachetable(void);
-void cache_do_readahead (block_sector_t next);
+void cache_read (block_sector_t bid, void * buffer, int offset, int readsize);
+void cache_write (block_sector_t bid, const void * buffer, int offset, int writesize);
+void cache_readahead (block_sector_t next);
 
-//for the read ahead thread
+
+/* read-ahead data structures */
+struct readahead
+{
+	block_sector_t bid;
+	struct list_elem elem;
+};
+
 struct lock lock_readahead;
 struct list list_readahead;
 struct condition cond_readahead;
-
-//structure for the readahead list
-struct readahead {
-
-        block_sector_t bid;
-        struct list_elem elem;
-
-};
 
 #endif /* filesys/cache.h */
 
